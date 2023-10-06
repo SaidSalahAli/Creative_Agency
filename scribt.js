416a717ac81df03059379c07adcc28217cc02331
SaidSalahAli <saidsalah375@gmail.com>
2023-10-06 21:01:52 +0300

    first commit

---
+++ scribt.js
@@ -0,0 +1,9 @@
+ function tran () {
+  let section = document.querySelector(".section-content",".section-img");
+  console.log('ss')
+  section.style.transform = "translateY(0)";
+  section.style.opacity = "1";
+};
+
+tran();
+console.log('said');
