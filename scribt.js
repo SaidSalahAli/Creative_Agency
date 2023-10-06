d77046ededcfd45372f577ac6dbe9dd7f427d81f
SaidSalahAli <saidsalah375@gmail.com>
2023-10-07 01:03:38 +0300

    Commit  create section2

--- scribt.js
+++ scribt.js
@@ -1,9 +1,7 @@
- function tran () {
-  let section = document.querySelector(".section-content",".section-img");
-  console.log('ss')
+function sectionAnimation() {
+  let section = document.querySelector(".section-content", ".section-img");
   section.style.transform = "translateY(0)";
   section.style.opacity = "1";
-};
+}
 
-tran();
-console.log('said');
+sectionAnimation();
