210311112e3087c4dd6d1d9a4a8d06bf20a2a804
SaidSalahAli <saidsalah375@gmail.com>
2023-10-08 00:43:34 +0300

    Commint create media query in section

--- scribt.js
+++ scribt.js
@@ -1,5 +1,5 @@
 function sectionAnimation() {
-  let section = document.querySelector(".section-content", ".section-img");
+  let section = document.querySelector(".section-content",);
   section.style.transform = "translateY(0)";
   section.style.opacity = "1";
 }
