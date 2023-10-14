b971dd806258c8131ab02fcb17afcd884990c516
SaidSalahAli <saidsalah375@gmail.com>
2023-10-14 22:22:19 +0300

    Commit finish

--- scribt.js
+++ scribt.js
@@ -1,7 +1,37 @@
-function sectionAnimation() {
-  let section = document.querySelector(".section-content",);
-  section.style.transform = "translateY(0)";
-  section.style.opacity = "1";
+function animateCountUp(targetElementId, endValue) {
+  let current = 0;
+  const element = document.getElementById(targetElementId);
+  const duration = 2000; // Animation duration in milliseconds
+
+  const step = (timestamp) => {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    current = Math.min(progress / duration, 1) * endValue;
+    element.textContent = Math.floor(current);
+
+    if (progress < duration) {
+      requestAnimationFrame(step);
+    }
+  };
+
+  let start = null;
+  requestAnimationFrame(step);
 }
 
-sectionAnimation();
+// Check when the section comes into the viewport
+const section5 = document.querySelector(".section5");
+
+const section5Observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Animate the numbers
+      animateCountUp("satisfiedClients", 92);
+      animateCountUp("totalProjects", 85);
+      animateCountUp("workCompleted1", 67);
+      animateCountUp("workCompleted2", 85);
+      section5Observer.unobserve(section5); // Stop observing once animation is triggered
+    }
+  });
+});
+
+section5Observer.observe(section5);
